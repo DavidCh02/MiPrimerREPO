b5851ed7e6c101f27ea1b3607cc2fd7d6ad1fd28
Cristopher David Chingo Rivera <166523123+DavidCh02@users.noreply.github.com>
2024-04-20 12:52:38 -0500

    agregar ejemplo 1

---
+++ ejemplo_1.py
@@ -0,0 +1,27 @@
+
+def imprimir_nombre(nombre, apellido):
+    nombre_completo = nombre + ' ' + apellido
+    print(nombre_completo)
+
+
+def cantidad_productos():
+    productos_comprados = int(input("¿Cuántos productos compró?: "))
+    return productos_comprados
+
+
+if __name__ == '__main__':
+    print("Bienvenido")
+
+    # Imprimir mi nombre completo
+    imprimir_nombre('Editado por: ', 'Cristopher')
+
+    # Ingresar cuantos productos compró en su compra
+    cantidad = cantidad_productos()
+    
+    # Imprimir la cantidad de productos comprados
+    print("Cantidad de productos comprados:", cantidad)
+
+    print("terminamos")
+
+
+
